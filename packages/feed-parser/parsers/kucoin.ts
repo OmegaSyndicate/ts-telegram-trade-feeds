dbc38a6cfc7653edd005356512fc53dd405d92bb
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2021-10-22 01:56:46 +0500

    fix: kucoin duplicates

--- packages/feed-parser/parsers/kucoin.ts
+++ packages/feed-parser/parsers/kucoin.ts
@@ -4,20 +4,21 @@ import { request } from "../helpers/request";
 
 export async function* sync(latestMessage, settings, logger) {
     while(true) {
-        console.log("test");
         const latest = (await latestMessage())?.value;
         yield await makeRequest(settings.token, latest);
     }
 }
 
-async function makeRequest(symbol, latest?) {
-    const received = (await request("GET", `https://api.kucoin.com/api/v1/market/histories`, { params: { symbol } })).data;
-    let offset = 0;
-    if(latest) {
-        const latestString = transactionString(JSON.parse(String(latest)));
-        offset = received.map(transactionString).indexOf(latestString) + 1;
+async function makeRequest(symbol, latestString?) {
+    const received: Transaction[] | [] = (await request("GET", `https://api.kucoin.com/api/v1/market/histories`, { params: { symbol } })).data;
+    let data: Transaction[] | [];
+    if(latestString) {
+        const latest: Transaction = JSON.parse(latestString);
+        data = received.filter((transaction) => Number(transaction.sequence) > Number(latest.sequence) && transaction.time >= latest.time);
+    } else {
+        data = received;
     }
-    return received.slice(offset)?.map(t => JSON.stringify(t));
+    return data.map(t => JSON.stringify(t));
 }
 
 interface Transaction {
@@ -26,8 +27,4 @@ interface Transaction {
     size: string;
     side: 'sell' | 'buy';
     time: number; // Timestamp of the transaction
-}
-
-function transactionString(t: Transaction) {
-    return t.sequence;
 }
