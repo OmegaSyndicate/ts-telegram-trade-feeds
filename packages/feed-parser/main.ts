be9f08caeca53c503060591b5ad6199fdb68dd6b
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2021-10-29 01:28:20 +0500

    fix(feed parser): Removed debug output

--- packages/feed-parser/main.ts
+++ packages/feed-parser/main.ts
@@ -18,7 +18,7 @@ interface IWorkers {
 let workers: {[key: string]: IWorkers} = {};
 
 function createWorker(token, key): Worker {
-    const worker = new Worker('./worker', { workerData: token, stdout: false })
+    const worker = new Worker('./worker', { workerData: token, stdout: true })
     const parserName = `parser-${key}`;
     worker.on('error', (err) => {
         logger.error(`${parserName}\n\n${err}`);
@@ -52,6 +52,5 @@ config.tokens.forEach(token => {
     }
 });
 
-console.log(workers);
 workerPoll(workers, logger);
 setInterval(workerPoll.bind(null, workers, logger), config.workerPoll * 1e3)
