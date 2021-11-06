aeca4db0f7ac8c0ff482734210470619fbcb985b
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2021-11-07 00:24:28 +0500

    hot fix bitfinex

--- packages/feed-telegram-bot/messageCreators/bitfinex.ts
+++ packages/feed-telegram-bot/messageCreators/bitfinex.ts
@@ -32,5 +32,5 @@ export function createMessage(options: Message, constants) {
     return `${options.type == "Bought" ? "🚀" : "👹"} *1 ${constants.token.substring(1,4)} = ${options.price.toFixed(4)} ${constants.token.substring(4)}T*\n`
     +   `${options.type} *${numWithCommas(Math.ceil(options.amount))} ${constants.token.substring(1,4)}* for *${numWithCommas(Math.ceil(options.amount * options.price))} ${constants.token.substring(4)}T* on Bitfinex\n\n`
     +   `${generateDots({ feedType: (options.type == "Bought" ? "uniswapBuy" : options.type), amountRadixInUsd: options.amount * options.price}, constants)}\n\n`
-    +   `💵 [Bitfinex](https://trade.kucoin.com/EXRD-USDT) | ℹ️ [Info](https://telegra.ph/Valar-List-of-informational-bots-03-23)`
+    +   `💵 [Bitfinex](https://trading.bitfinex.com/t/XRDUSD?type=exchange) | ℹ️ [Info](https://telegra.ph/Valar-List-of-informational-bots-03-23)`
 }
