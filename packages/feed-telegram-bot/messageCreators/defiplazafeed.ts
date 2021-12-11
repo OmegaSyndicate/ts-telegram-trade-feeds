7a500a79ee2f179affe76921278f57f05f6333ca
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2021-12-11 08:45:25 +0500

    fix: defiplazafeed

--- packages/feed-telegram-bot/messageCreators/defiplazafeed.ts
+++ packages/feed-telegram-bot/messageCreators/defiplazafeed.ts
@@ -11,7 +11,7 @@ export function createMessage(options: any, constants) {
         const otherAmount = Number(options.outputToken.symbol == constants.token ? options.inputAmount : options.outputAmount);
         const otherName = (options.outputToken.symbol == constants.token ? options.inputToken.symbol : options.outputToken.symbol).toUpperCase();
         return `${options.type == "Bought" ? "🚀" : "👹"} *1 ${constants.token.toUpperCase()} = ${dfpInUsd.toFixed(4)} USD*\n` +
-                `${options.type} *${numWithCommas(Math.floor(dfpAmount * 1000) / 1000)} ${constants.token.toUpperCase()}* for *${numWithCommas(Math.floor(otherAmount * 1000) / 1000)} ${otherName}* on Defi Plaza (Gas Fee: $${numWithCommas(Math.ceil(gasFee))})\n\n` +
+                `${options.type} *${numWithCommas(Math.floor(dfpAmount * 1000) / 1000)} ${constants.token.toUpperCase()}* for *${numWithCommas(Math.floor(otherAmount * 1000) / 1000)} ${otherName}* on DefiPlaza (Gas Fee: $${numWithCommas(Math.ceil(gasFee))})\n\n` +
                 `${generateDots(options.swapUSD, constants, options.type == "Bought" ? "🟢" : "🔴" )}\n\n` +
                 `From address: [${shortenAddress(options.sender)}](${createEtherscanLink("address", options.sender)})\n\n` +
                 `🏛 [Defi Plaza](https://defiplaza.net/swap) | 📶 [Tx Hash](${createEtherscanLink("tx", options.id)}) | 📊 [Dextools](https://www.dextools.io/app/ether/pair-explorer/0x820d74078eb4c94e24ef0bcc8ccf848a238f473e) | 💥 [Powered by CERBY Token](https://t.me/CerbyToken)`
