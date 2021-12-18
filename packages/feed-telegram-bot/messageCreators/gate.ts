33bbd84860403d7e211a78bf9be7ee2ef2b68b9e
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2021-12-18 14:43:58 +0500

    chore: Update price digit

--- packages/feed-telegram-bot/messageCreators/gate.ts
+++ packages/feed-telegram-bot/messageCreators/gate.ts
@@ -15,7 +15,7 @@ export function createMessage(options: Message, constants) {
     console.log(options);
     let symbol = options.currency_pair.split('_')[1];
     let swapInUsd = options.anotherPrice ? (options.anotherPrice * +options.price) * +options.amount : +options.amount * +options.price
-    return `${options.side == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${options.anotherPrice ? `${(options.anotherPrice * +options.price).toFixed(2)} USD (${(+options.price).toFixed(6)} ${symbol})` : `${(+options.price).toFixed(2)} ${symbol}`}*\n`
+    return `${options.side == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${options.anotherPrice ? `${(options.anotherPrice * +options.price).toFixed(constants.priceDigit)} USD (${(+options.price).toFixed(6)} ${symbol})` : `${(+options.price).toFixed(constants.priceDigit)} ${symbol}`}*\n`
     +   `${options.side == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.ceil(+options.amount))} ${constants.token}* for *${options.anotherPrice ? (+options.amount * +options.price).toFixed(4) : numWithCommas(Math.ceil(+options.amount * +options.price))} ${symbol}* on Gate\n\n`
     +   `${generateDots({ feedType: (options.side == "buy" ? "uniswapBuy" : options.side), amountRadixInUsd: swapInUsd}, constants)}\n\n`
     +   `${constants.mainLink} | 🚪 [Gate.io](${constants.tradeLinks[symbol]}) | 💥 [Powered by CERBY Token](https://t.me/CerbyToken)`
--- packages/feed-telegram-bot/messageCreators/okex.ts
+++ packages/feed-telegram-bot/messageCreators/okex.ts
@@ -11,7 +11,7 @@ export interface Message {
 
 export function createMessage(options: Message, constants) {
     let symbol = options.instId.split('-')[1];
-    return `${options.side == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${(+options.px).toFixed(2)} ${symbol}*\n`
+    return `${options.side == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${(+options.px).toFixed(constants.priceDigit)} ${symbol}*\n`
     +   `${options.side == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.ceil(+options.sz))} ${constants.token}* for *${numWithCommas(Math.ceil(+options.sz * +options.px))} ${symbol}* on Okex\n\n`
     +   `${generateDots({ feedType: (options.side == "buy" ? "uniswapBuy" : options.side), amountRadixInUsd: +options.sz * +options.px}, constants)}\n\n`
     +   `${constants.mainLink} | 👌 [Okex](${constants.tradeLinks[symbol]}) | 💥 [Powered by CERBY Token](https://t.me/CerbyToken)`
