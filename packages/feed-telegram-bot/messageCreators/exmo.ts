4ac588607b4e430a129058d2bdc70f0618eefbc2
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2021-12-22 04:37:48 +0500

    chore: update message for gate and exmo

--- packages/feed-telegram-bot/messageCreators/exmo.ts
+++ packages/feed-telegram-bot/messageCreators/exmo.ts
@@ -15,9 +15,9 @@ export interface Message {
 export function createMessage(options: Message, constants) {
     let symbol = options.pair.split('_')[1];
     console.log(options);
-    let swapInUsd = options.anotherPrice ? (options.anotherPrice * +options.price) * +options.amount : +options.amount * +options.price
+    let swapInUsd = options.anotherPrice ? +options.amount * options.anotherPrice : +options.amount
     return `${options.type == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${options.anotherPrice ? `${(options.anotherPrice * +options.price).toFixed(constants.priceDigit)} USD (${(+options.price).toFixed(6)} ${symbol})` : `${(+options.price).toFixed(constants.priceDigit)} ${symbol}`}*\n`
-    +   `${options.type == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.ceil(+options.quantity))} ${constants.token}* for *${ options.anotherPrice ?  (+options.amount).toFixed(6) : numWithCommas(Math.ceil(+options.amount))} ${symbol}* on EXMO\n\n`
+    +   `${options.type == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.ceil(+options.quantity))} ${constants.token}* for *${ options.anotherPrice ?  (+options.amount).toFixed(6) : numWithCommas(Math.ceil(+options.amount))} ${symbol}${options.anotherPrice ? ` (${numWithCommas(Math.ceil(swapInUsd))}$)` : ''}* on EXMO\n\n`
     +   `${generateDots({ feedType: (options.type == "buy" ? "uniswapBuy" : options.type), amountRadixInUsd: swapInUsd}, constants)}\n\n`
     +   `${constants.mainLink} | ✖️ [EXMO](${constants.tradeLinks[symbol]}) | 💥 [Powered by CERBY Token](https://t.me/CerbyToken)`
 }
--- packages/feed-telegram-bot/messageCreators/exmo_ru.ts
+++ packages/feed-telegram-bot/messageCreators/exmo_ru.ts
@@ -15,9 +15,9 @@ export interface Message {
 export function createMessage(options: Message, constants) {
     let symbol = options.pair.split('_')[1];
     console.log(options);
-    let swapInUsd = options.anotherPrice ? (options.anotherPrice * +options.price) * +options.amount : +options.amount * +options.price
+    let swapInUsd = options.anotherPrice ? +options.amount * options.anotherPrice : +options.amount
     return `${options.type == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${options.anotherPrice ? `${(options.anotherPrice * +options.price).toFixed(2)} USD (${(+options.price).toFixed(6)} ${symbol})` : `${(+options.price).toFixed(2)} ${symbol}`}*\n`
-    +   `${options.type == "buy" ? "Покупка" : "Продажа"} *${numWithCommas(Math.ceil(+options.quantity))} ${constants.token}* за *${ options.anotherPrice ?  (+options.amount).toFixed(6) : numWithCommas(Math.ceil(+options.amount))} ${symbol}* на EXMO\n\n`
+    +   `${options.type == "buy" ? "Покупка" : "Продажа"} *${numWithCommas(Math.ceil(+options.quantity))} ${constants.token}* за *${ options.anotherPrice ?  (+options.amount).toFixed(6) : numWithCommas(Math.ceil(+options.amount))} ${symbol}${options.anotherPrice ? ` (${numWithCommas(Math.ceil(swapInUsd))}$)` : ''}* на EXMO\n\n`
     +   `${generateDots({ feedType: (options.type == "buy" ? "uniswapBuy" : options.type), amountRadixInUsd: swapInUsd}, constants)}\n\n`
     +   `${constants.mainLink} | ✖️ [EXMO](${constants.tradeLinks[symbol]}) | 💥 [При поддержке CERBY Token](https://t.me/CerbyToken)`
 }
