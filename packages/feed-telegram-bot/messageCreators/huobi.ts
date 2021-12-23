99f7ba408c0606cd7323abdbf815967360956072
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2021-12-23 20:41:48 +0500

    fix

--- packages/feed-telegram-bot/messageCreators/huobi.ts
+++ packages/feed-telegram-bot/messageCreators/huobi.ts
@@ -10,7 +10,7 @@ export interface Message {
 }
 
 export function createMessage(options: Message, constants) {
-    let symbol = constants.pair;
+    let symbol = constants.currencypair;
     return `${options.direction == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${(+options.price).toFixed(4)} ${symbol}*\n`
     +   `${options.direction == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.ceil(+options.amount))} ${constants.token}* for *${numWithCommas(Math.ceil(+options.amount * +options.price))} ${symbol}* on Huobi\n\n`
     +   `${generateDots({ feedType: (options.direction == "buy" ? "uniswapBuy" : options.direction), amountRadixInUsd: +options.amount * +options.price}, constants)}\n\n`
