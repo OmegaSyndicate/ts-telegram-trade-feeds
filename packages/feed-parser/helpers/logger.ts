5ae266c4e2f1b65b4c1c33f51ced2cede51cc774
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2022-02-15 02:43:40 +0500

    fix: In some cases, the error message was displayed incorrectly.

--- packages/feed-parser/helpers/logger.ts
+++ packages/feed-parser/helpers/logger.ts
@@ -8,21 +8,21 @@ export class Logger {
         this.sendMessage = sendMessage;
     }
     log(message: string | Object) {
-        if(message instanceof Object) {
+        if(message instanceof Object && !(message instanceof Error)) {
             message = JSON.stringify(message, null, 2);
         }
         console.log(message);
         return this.sendMessage([`Log at ${Date()} from ${this.info}\n${message}`]);
     }
     warn(message: string | Object) {
-        if(message instanceof Object) {
+        if(message instanceof Object && !(message instanceof Error)) {
             message = JSON.stringify(message, null, 2);
         }
         console.warn(message);
         return this.sendMessage([`Warn at ${Date()} from ${this.info}\n${message}\n\nStack trace: ${new Error().stack}`]);
     }
     error(message: string | Object) {
-        if(message instanceof Object) {
+        if(message instanceof Object && !(message instanceof Error)) {
             message = JSON.stringify(message, null, 2);
         }
         console.error(message);
