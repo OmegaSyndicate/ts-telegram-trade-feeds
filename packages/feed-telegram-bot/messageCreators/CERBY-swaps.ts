dbbbede0b37d5bc811b27100db292c9e39a33b01
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2022-03-31 18:04:36 +0700

    chore: Uniswap -> Uniswap v3

--- packages/feed-telegram-bot/messageCreators/CERBY-swaps.ts
+++ packages/feed-telegram-bot/messageCreators/CERBY-swaps.ts
@@ -21,7 +21,7 @@ export interface Message {
 export function createMessage(options: any, constants) {
     options.deftInUsd = +options.amountDeftInUsd / +options.amountDeft;
      return `${options.feedType == "buy" ? "🚀" : "👹"} *1 CERBY = ${Number(options.deftInUsd).toFixed(7)} ${constants.token.toUpperCase()}*\n` +
-            `${options.feedType == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.floor(options.amountDeft * 1000) / 1000)} CERBY* for *${numWithCommas(Math.ceil(options.amountDeftInUsd * 1000) / 1000)} ${constants.token.toUpperCase()}* on ${constants.stakingType} (Gas Fee: $${numWithCommas(Math.ceil(options.transactionFeeInUsd))})\n\n` +
+            `${options.feedType == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.floor(options.amountDeft * 1000) / 1000)} CERBY* for *${numWithCommas(Math.ceil(options.amountDeftInUsd * 1000) / 1000)} ${constants.token.toUpperCase()}* on ${constants.stakingType == "Uniswap" ? "Uniswap V3" : constants.stakingType} (Gas Fee: $${numWithCommas(Math.ceil(options.transactionFeeInUsd))})\n\n` +
             `${generateDots(options.amountDeftInUsd, constants, options.feedType == "buy" ? "🟢" : "🔴")}\n\n` +
             `From address: [${shortenAddress(options.from)}](${constants.scanURL}address/${options.from})\n\n` +
             `${constants.chain} | 📶 [Tx Hash](${constants.scanURL}tx/${options.txHash}) ${constants.DexToolsLink ? `| 📊 [Dextools](${constants.DexToolsLink}) ` : ''}| 💥 [Powered by Cerby Finance](https://cerby.fi)`
