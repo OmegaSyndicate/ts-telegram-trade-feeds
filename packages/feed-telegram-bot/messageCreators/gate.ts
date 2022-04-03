f5584d5f9800d3f2af799dbcc2ccf8656b23babb
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2022-04-03 14:33:52 +0700

    fix

--- packages/feed-telegram-bot/messageCreators/gate.ts
+++ packages/feed-telegram-bot/messageCreators/gate.ts
@@ -17,6 +17,6 @@ export function createMessage(options: Message, constants) {
     let swapInUsd = options.anotherPrice ? (options.anotherPrice * +options.price) * +options.amount : +options.amount * +options.price
     return `${options.side == "buy" ? "🚀" : "👹"} *1 ${constants.token} = ${options.anotherPrice ? `${(options.anotherPrice * +options.price).toFixed(constants.priceDigit)} USD (${(+options.price).toFixed(6)} ${symbol})` : `${(+options.price).toFixed(constants.priceDigit)} ${symbol}`}*\n`
     +   `${options.side == "buy" ? "Bought" : "Sold"} *${numWithCommas(Math.ceil(+options.amount))} ${constants.token}* for *${options.anotherPrice ? (+options.amount * +options.price).toFixed(4) : numWithCommas(Math.ceil(+options.amount * +options.price))} ${symbol}${options.anotherPrice ? ` (${numWithCommas(Math.ceil(swapInUsd))}$)` : ''}* on Gate\n\n`
-    +   `${generateDots(swapInUsd, constants, options.side == "buy" ? "🟢" : "🔴")}`
+    +   `${generateDots(swapInUsd, constants, options.side == "buy" ? "🟢" : "🔴")}\n\n`
     +   `${constants.mainLink} | 🚪 [Gate.io](${constants.tradeLinks[symbol]}) | ${CerbyFinance}`
 }
