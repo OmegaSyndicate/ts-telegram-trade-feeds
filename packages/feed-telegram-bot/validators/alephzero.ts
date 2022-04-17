04d08010c67825d6215dd5bdabfdd8db2777daa5
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2022-04-17 15:36:11 +0700

    feat: Added the ability to disable messages about deposits/withdrawals.

--- packages/feed-telegram-bot/validators/alephzero.ts
+++ packages/feed-telegram-bot/validators/alephzero.ts
@@ -1,5 +1,8 @@
 import { Message } from '../messageCreators/alephzero';
 
 export function validate(config, msg: Message): boolean {
+    if((config.noDeposit && msg.feedType == 'deposit') || (config.noWithDraw && msg.feedType == 'withdraw')) {
+        return false; 
+    }
     return +msg.amount >= config.minALEPH;
 }
