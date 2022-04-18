a3b98945faeccbfda88f8b927f2b65486c887892
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2022-04-18 15:38:07 +0700

    fix

--- packages/feed-telegram-bot/validators/alephzero.ts
+++ packages/feed-telegram-bot/validators/alephzero.ts
@@ -2,8 +2,8 @@ import { Message } from '../messageCreators/alephzero';
 
 export function validate(config, msg: Message): boolean {
     if(msg.feedType == 'deposit'
-       ? ( config.noDeposit  || (config.noDepositFrom  && (config.noDepositFrom  as string[]).includes(msg.from.toLowerCase())) )
-       : ( config.noWithDraw || (config.noWithDrawFrom && (config.noWithDrawFrom as string[]).includes(msg.to.toLowerCase())) )
+       ? ( config.noDeposit  || (config.noDepositFrom  && (config.noDepositFrom  as string[]).includes(msg.from)))
+       : ( config.noWithDraw || (config.noWithDrawFrom && (config.noWithDrawFrom as string[]).includes(msg.to)))
     ) {
         return false; 
     }
