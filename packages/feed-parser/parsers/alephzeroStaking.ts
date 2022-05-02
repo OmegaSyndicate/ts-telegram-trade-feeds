41ebbbe55b4371af1556db86230ac0a4935ad6f9
Sergey Scheglov <oxdeadbeef.ssch@gmail.com>
2022-05-02 07:00:55 +0700

    feat: added bond_extra in aleph staking

--- packages/feed-parser/parsers/alephzeroStaking.ts
+++ packages/feed-parser/parsers/alephzeroStaking.ts
@@ -1,9 +1,7 @@
-import { Logger } from "../helpers/logger";
 import { request } from "../helpers/request";
 
 const apiURL = "https://alephzero.api.subscan.io/";
 const apiKey = "03f7cf1c0d0741aed2be3cfb53855f9c";
-
 
 export async function* sync(latestMessage, settings, logger) {
     let latestSaved, data;
@@ -17,7 +15,12 @@ export async function* sync(latestMessage, settings, logger) {
                 await new Promise((resolve) => setTimeout(resolve, 60000));
                 throw new Error("The received last saved transaction from kafka does not match the one saved in the current instance.");
             }
-            yield data = (normalization(await makeRequest(latest ? JSON.parse(String(latest)).extrinsic_index : undefined))).map(t => JSON.stringify(t));
+            const bond = (await makeRequest('bond', logger)) as stake[];
+            const bond_extra = (await makeRequest('bond_extra', logger)) as extrastake[];
+            yield data = (await normalization(
+                mergeTransactions(bond, bond_extra, latestSaved),
+                latest ? JSON.parse(String(latest)).extrinsic_index : undefined
+            )).map(t => JSON.stringify(t));
             if(data.length) {
                 latestSaved = JSON.stringify(data.slice(-1));
             }
@@ -29,39 +32,104 @@ export async function* sync(latestMessage, settings, logger) {
     }
 }
 
-export async function makeRequest(extrinsic_index?: string, logger?): Promise<stake[]> {
+export async function makeRequest(type: 'bond_extra' | 'bond', logger?): Promise<stake[] | extrastake[]> {
     const response = await request("POST", apiURL + 'api/scan/extrinsics', {
-        data_raw: `{ "row": 100, "page": 0, "module": "staking", "call": "bond"}`,
+        data_raw: `{ "row": 100, "page": 0, "module": "staking", "call": "${type}"}`,
         headers: {
             "Content-Type": "application/json",
             "X-API-Key": apiKey
         }
     });
-    const price = (await request('GET', "https://api.coingecko.com/api/v3/simple/price", { params: {
-        ids: "aleph-zero",
-        vs_currencies: "usd"
-    }}))['aleph-zero'].usd;
     if(response.code != 0) {
         throw response;
     }
-    const extrinsics: stake[] = response.data.extrinsics.map((extrinsic) => {
-        return { ...extrinsic, price, amount: +JSON.parse(extrinsic.params)[1].value / 1e12 }
-    }).reverse();
-    if(!extrinsic_index) {
-        return extrinsics;
+    return response.data.extrinsics.reverse();
+}
+
+export async function normalization(extrs: ReturnType<typeof mergeTransactions>, extrinsic_index?: string): Promise<(stake | extrastake)[]> {
+    const extrinsics = extrs.filter((extrinsic) => extrinsic.extrinsic_index > extrinsic_index);
+    if(extrinsics.length) {
+        const price = (await request('GET', "https://api.coingecko.com/api/v3/simple/price", { params: {
+            ids: "aleph-zero",
+            vs_currencies: "usd"
+        }}))['aleph-zero'].usd;
+        return extrinsics.map((extrinsic: stake | extrastake) => {
+            console.log(extrinsic);
+            extrinsic.feedType = "stakeStarted";
+            extrinsic.price = price;
+            extrinsic.amount = 
+                extrinsic.call_module_function == 'bond'
+                ? +JSON.parse(extrinsic.params)[1].value / 1e12
+                : +JSON.parse(extrinsic.params)[0].value / 1e12
+            return extrinsic;
+        });
     } else {
-        return extrinsics.filter((extrinsic) => extrinsic.extrinsic_index > extrinsic_index);
+        return [];
     }
 }
 
-export function normalization(extrinsics: stake[]): stake[] {
-    return extrinsics.map((extrinsic) => {
-        extrinsic.feedType = "stakeStarted"
-        return extrinsic;
-    });
+
+function mergeTransactions(first: stake[], two: extrastake[], latestSaved): (stake | extrastake)[] {
+    let i, j;
+    i = j = 0;
+    let n = first.length;
+    let m = two.length;
+    let result = [];
+    while (i < n && j < m) {
+        if (first[i].extrinsic_index < two[j].extrinsic_index) {
+            result.push(first[i++]);
+        } else {
+            result.push(two[j++]);
+        }
+    }
+    while (i < n) {
+        result.push(first[i++]);
+    }
+    while (j < m) {
+        result.push(two[j++]);
+    }
+    if(latestSaved) {
+        const latestTHash: (stake | extrastake)['extrinsic_index'] = JSON.parse(String(latestSaved)).tid;
+        result.forEach((transaction: (stake | extrastake)) => {
+            if(transaction.extrinsic_index == latestTHash) {
+                throw new Error("Duplicate transaction found during merge.\n"
+                                + `latestSaved: ${latestSaved}\n`
+                                + `received: ${JSON.stringify({first, two})}\n`
+                                + `with merge: ${JSON.stringify(result)}\n`);
+            }
+        })
+    }
+    return result;
 }
 
-
+interface extrastake {
+    amount: number,
+    feedType: "stakeStarted",
+    price: number,
+    block_timestamp: number // 1651415980,
+    block_num: number //14872563,
+    extrinsic_index: string //'14872563-1',
+    call_module_function: 'bond_extra',
+    call_module: 'staking',
+    params: string //'[{"name":"max_additional","type":"compact\\u003cU128\\u003e","type_name":"BalanceOf","value":"4850000000000000"}]',
+    account_id: string //'5F9qHWiP4ZbiJsbt2QHaHGyNqAQUaU43J8J9SCaNTop9MypN',
+    account_index: string //'',
+    signature: string //'0x2caeb5db5ee79624876cb849f55630793206b7e60e62b2b2f11d445df8ca0d2a7315be777dff1a816433a42e335373e240babab71e922dbea11abb563317678c',
+    nonce: number //9,
+    extrinsic_hash: string //'0xbf2994ecb8d35f40ded7807cc37795d27e9a2dc1086075f2d0485b8a56336b96',
+    success: boolean // true,
+    fee: string // '1139296115',
+    from_hex: string //'889deef5e8112a89e4f2d973b9250c8c5b44427d31f52558fe40c88b42bfb553',
+    finalized: boolean,
+    account_display: {
+        address: string // "5CnnXNMWk2PR5839SWVueWJAXSaLWHwV6fF3yxmhM4yHz1BB",
+        display: string //"",
+        judgements: string | null //null,
+        account_index: string// "",
+        identity: boolean // false,
+        parent: string | null
+    }
+}
 interface stake {
     amount: number,
     feedType: "stakeStarted",
