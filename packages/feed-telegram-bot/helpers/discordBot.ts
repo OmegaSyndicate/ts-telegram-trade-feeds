df144aa5e511ba7dc234914dba8da5286d016e45
Trebel <trebel@defiplaza.net>
2022-06-07 19:03:28 +0200

    fix: remove discord bot due to compile errors

--- packages/feed-telegram-bot/helpers/discordBot.ts
+++ packages/feed-telegram-bot/helpers/discordBot.ts
@@ -1,56 +1,56 @@
-// import { discordToken } from '../../config.json'
-import * as Discord from "discord.js";
+// // import { discordToken } from '../../config.json'
+// import * as Discord from "discord.js";
 
 
-// (async () => {
-//     const client = new Discord.Client({
-//         intents: [Discord.Intents.FLAGS.GUILDS]
-//     });
+// // (async () => {
+// //     const client = new Discord.Client({
+// //         intents: [Discord.Intents.FLAGS.GUILDS]
+// //     });
 
-//     console.log(await client.login(discordToken));
+// //     console.log(await client.login(discordToken));
 
-//     await new Promise(r => client.once('ready', r));
+// //     await new Promise(r => client.once('ready', r));
 
-//     // let channel = await client.guilds.channels.cache.get("937131976677294091") as Discord.TextChannel
-//     let channel = client.channels.cache.get('937131976677294091') as Discord.TextChannel;
-//     for(let i = 0; i < 5; i++) {
-//         await channel.send("This is (test)[http://google.com] on **markdown** тест " + i);
-//         await new Promise(r => setTimeout(r, 1000));
+// //     // let channel = await client.guilds.channels.cache.get("937131976677294091") as Discord.TextChannel
+// //     let channel = client.channels.cache.get('937131976677294091') as Discord.TextChannel;
+// //     for(let i = 0; i < 5; i++) {
+// //         await channel.send("This is (test)[http://google.com] on **markdown** тест " + i);
+// //         await new Promise(r => setTimeout(r, 1000));
+// //     }
+
+
+// // })()
+
+
+// export class DiscordBot {
+//     bot: Discord.Client;
+//     channel: Promise<Discord.TextChannel>;
+
+//     logger;
+//     markdownEnable: boolean;
+//     constructor(botToken, channel, logger, amountPubs, markdownEnable = true) {
+//         this.bot = new Discord.Client({
+//             intents: [Discord.Intents.FLAGS.GUILDS]
+//         });
+//         this.bot.login(botToken);
+//         this.channel = new Promise(r => {
+//             this.bot.once('ready', () => {
+//                 r(this.bot.channels.cache.get(channel) as Discord.TextChannel)
+//             })
+//         })
+
 //     }
-
-
-// })()
-
-
-export class DiscordBot {
-    bot: Discord.Client;
-    channel: Promise<Discord.TextChannel>;
-
-    logger;
-    markdownEnable: boolean;
-    constructor(botToken, channel, logger, amountPubs, markdownEnable = true) {
-        this.bot = new Discord.Client({
-            intents: [Discord.Intents.FLAGS.GUILDS]
-        });
-        this.bot.login(botToken);
-        this.channel = new Promise(r => {
-            this.bot.once('ready', () => {
-                r(this.bot.channels.cache.get(channel) as Discord.TextChannel)
-            })
-        })
-
-    }
-    async sendMessage(message: string) {
-        if(!message) return;
-        // let messageArray = message.replace(/\*/gi, '**').split('\n');
-        (await this.channel).send({
-            embeds: [
-                {
-                    // title: messageArray.splice(1,1).join(''),
-                    description: message // messageArray.join('\n')
-                }
-            ]
-        });
-        return 0;
-    }
-}
+//     async sendMessage(message: string) {
+//         if(!message) return;
+//         // let messageArray = message.replace(/\*/gi, '**').split('\n');
+//         (await this.channel).send({
+//             embeds: [
+//                 {
+//                     // title: messageArray.splice(1,1).join(''),
+//                     description: message // messageArray.join('\n')
+//                 }
+//             ]
+//         });
+//         return 0;
+//     }
+// }
